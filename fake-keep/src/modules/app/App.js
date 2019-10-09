3baced4b9f2440f088563cb37838276f23b51f3e
Nayara Souza <nsouza@thoughtworks.com>
2019-10-08 21:25:21 -0300

    Creates a navbar component with props

--- fake-keep/src/modules/app/App.js
+++ fake-keep/src/modules/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../header/Header';
+import NavBar from '../navBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NavBar titles={["Notas", "Criar Nota"]}/>
     </div>
   );
 }
---
+++ fake-keep/src/modules/navBar/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        {props.titles.map((title, index) => (
+          <li key={index}>
+            <a href="/#">{title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
