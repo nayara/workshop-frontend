ade337b29ab84f6ae1645e1d25d9b77f41b7d92f
Nayara Souza <nsouza@thoughtworks.com>
2019-10-14 10:04:29 -0300

    Styling list component

--- fake-keep/src/modules/todo-list/components/task-list/TaskList.js
+++ fake-keep/src/modules/todo-list/components/task-list/TaskList.js
@@ -6,12 +6,17 @@ import Task from '../task/Task';
 class TaskList extends Component {
   render(){
     return(
-    <article>
-      <header>
-        <h1 className="task-list-title">Área e sala</h1>
-      </header>
-      <Task />
-    </article>
+    <div>
+      <header className="task-list-title">Notas</header>
+      <article className="task-list-box">
+        <header>
+          <h1 className="task-list-box-title">Área e sala</h1>
+        </header>
+        <ul className="task-list">
+          <Task />
+        </ul>
+      </article>
+    </div>
     );
   };
 }
--- fake-keep/src/modules/todo-list/components/task/Task.js
+++ fake-keep/src/modules/todo-list/components/task/Task.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './task.css'
+
 function Task() {
   return(
     <label className="task">
