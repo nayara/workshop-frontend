dc4f0ef0ef5381d7970fd754c975d48cbc1595ef
Nayara Souza <nsouza@thoughtworks.com>
2020-04-20 19:00:56 -0300

    Fix application imports

--- fake-keep/src/modules/app/App.js
+++ fake-keep/src/modules/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../header/Header';
-import NavBar from '../navbar/Navbar';
+import NavBar from '../navBar/NavBar';
 import TaskList from '../todo-list/components/task-list/TaskList';
 
 import "./app.css";
--- fake-keep/src/modules/navBar/NavBar.js
+++ fake-keep/src/modules/navBar/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './navbar.css';
+import './NavBar.css';
 
 function NavBar(props) {
   return (
